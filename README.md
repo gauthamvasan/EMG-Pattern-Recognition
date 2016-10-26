df0d80e241af1688295b01456b3d8b0348cbadd4
Gautham Vasan <gauthamv.529@gmail.com>
2016-10-25 23:38:11 -0600

    Init files

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Gesture recognition using the NINAPRO Dataset
 
 Using data acquired using 10 Otto Myoblock elecrodes/Delsys Trigno Wireless systems and CyberGlove II 
+Each matrix has 6 categories - emg, repetition, restimulus, stimulus, glove, rerepetition
--- svm_rff.py
+++ svm_rff.py
@@ -1,4 +1,6 @@
 # Gesture recognition using SVM with Random Fourier Features
+# This script loads every single mat file listed in the data directory.
+# Each matrix has 6 categories - emg, repetition, restimulus, stimulus, glove, rerepetition
 import os
 import numpy as np
 import scipy.io
@@ -7,6 +9,6 @@ dataDir = "/home/gautham/EMG-Pattern-Recognition/NINAPRO_Dataset/Database 1/Exer
 mats = []
 for file in os.listdir( dataDir ) :
     mats.append( scipy.io.loadmat( dataDir+file ) )
-print(len(mats))
+print mats[0]
 print (mats[0]['stimulus'].shape)
-print (mats[0]['stimulus'][1000])
+print (max(mats[0]['restimulus']))
